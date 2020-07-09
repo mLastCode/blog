046270f22b187909a8cedea208978d7945cde34d
lastcode <790347573@qq.com>
2020-07-09 18:36:31 +0800

    '数据库配置'

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'USER': 'maik',
+        'PASSWORD': '123456',
+        'NAME': 'blog',
+    },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
