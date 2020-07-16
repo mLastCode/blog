849d07aa5f108826a5654a94de53c33ecb51bb27
lastcode <790347573@qq.com>
2020-07-17 02:21:42 +0800

    图片验证码配置

--- blog/users/models.py
+++ blog/users/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+# 用户信息
+class User(AbstractUser):
+
+    # 电话号码
+    mobile = models.CharField(max_length=11, unique=True, blank=True)
+
+    # 头像信息
+    avatar = models.ImageField(upload_to='avatar/%Y%m%d/', blank=True)
+
+    # 个人简介
+    user_desc = models.TextField(max_length=500, blank=True)
+
+    # 修改认证的字段
+    USERNAME_FIELD = 'mobile'
+
+    # 创建超级管理员的需要必须输入的字段
+    REQUIRED_FIELDS = ['username', 'email']
+
+    # 内部类 class Meta 用于给 model 定义元数据
+    class Meta:
+        db_table = 'tb_user'    # 修改表名
+        verbose_name = '用户信息'   # admin 后台显示
+        verbose_name_plural = verbose_name  # admin 后台显示
+
+    def __str__(self):
+        return self.mobile
--- blog/users/views.py
+++ blog/users/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views import View
 # Create your views here.
 
+from django.http.response import HttpResponseBadRequest, HttpResponse
+from libs.captcha.captcha import captcha
+from django_redis import get_redis_connection
 
 # 注册视图
 class RegisterView(View):
@@ -9,3 +12,21 @@ class RegisterView(View):
     def get(self, request):
 
         return render(request, 'register.html')
+
+
+# 图片验证码视图
+class ImageCodeView(View):
+
+    def get(self, request):
+
+        uuid = request.GET.get('uuid')
+
+        if uuid is None:
+            return HttpResponseBadRequest("请求参数错误")
+
+        text, image = captcha.generate_captcha()
+
+        redis_conn = get_redis_connection('default')
+        redis_conn.setex = ('img: %s' % uuid, 300, text)
+
+        return HttpResponse(image, content_type='image/jpg')
